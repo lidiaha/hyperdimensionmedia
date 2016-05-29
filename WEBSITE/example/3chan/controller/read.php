856e734c7f7807a0be4b5c2939b454a5243597b3
agentOfChaos <informaniac99@gmail.com>
2016-05-29 21:27:58 +0200

    added an example to showcase jquery, ajax and stuff. go to example/index.html and see ;) (also, re-import the database)

---
+++ WEBSITE/example/3chan/controller/read.php
@@ -0,0 +1,21 @@
+<?php
+  include "../../phplib/database.php";
+  $conn = dbconn();
+  if (isset($_POST["after"])) {
+    $sql = "SELECT * FROM testchat WHERE time > ?";
+    $prepared = $conn->prepare($sql);
+    $prepared->bind_param("s", $_POST["after"]);
+    $prepared->execute();
+    $result = $prepared->get_result();
+  }
+  else {
+    $sql = "SELECT * FROM testchat";
+    $result = $conn->query($sql);
+  }
+
+  $rows = array();
+  while($r = mysqli_fetch_assoc($result)) {
+      $rows[] = $r;
+  }
+  print json_encode($rows);
+ ?>
