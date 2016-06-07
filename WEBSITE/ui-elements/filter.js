ccbbd482ecf11ea6ca26507bed7de6fe77f0ecc5
lidiaha <lidia_93@alice.it>
2016-06-07 19:28:30 +0200

    filter 2.0 js

--- WEBSITE/ui-elements/filter.js
+++ WEBSITE/ui-elements/filter.js
@@ -1,16 +1,18 @@
 <link rel="stylesheet" type="text/css" href="style/filter.css">
 <script> 
-//Dichiaro la funzione 
+//Dichiaro la funzione che fa scomparire cose
 function azione(variabile) { 
-if(document.getElementById(variabile).style.display=='') { 
-document.getElementById(variabile).style.display='none'; 
-}else{ 
-document.getElementById(variabile).style.display=''; 
-} 
+   if(document.getElementById(variabile).style.display=='block') { 
+      document.getElementById(variabile).style.display='none'; 
+   }else{ 
+      document.getElementById(variabile).style.display='block'; 
+   } 
+}
 </script>
+
 <div class="filter">
    <div class="cate">
-      <a href="#sub-1" onClick="azione('sub-1')> <div class="sub">Prezzo </div></a>
+      <a href="#sub-1" onClick="azione('sub-1');"> <div class="sub">Prezzo </div></a>
       <div id="sub-1" class= "element">
          <input class= "item" type="checkbox" name="price" value="50-150 €">50-150 €<br>
          <input class= "item" type="checkbox" name="price" value="150-200 €">150-200 €<br>
@@ -18,7 +20,7 @@ document.getElementById(variabile).style.display='';
       </div>
    </div>
    <div class="cate">
-      <a href="#sub-2"> <div class="sub">Marca </div></a>
+      <a href="#" onClick="azione('sub-2');"> <div class="sub">Marca </div></a>
       <div id="sub-2" class= "element">
          <input class= "item" type="checkbox" name="brand" value="Shitphone">Shitphone<br>
          <input class= "item" type="checkbox" name="brand" value="Iphone">Iphone<br>
