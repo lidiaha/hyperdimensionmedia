b6c24accffef96c23ba58fcdf99d34d21f290127
lidiaha <lidia_93@alice.it>
2016-06-08 21:14:31 +0200

    update device-presentation

--- WEBSITE/pages/device-presentation.php
+++ WEBSITE/pages/device-presentation.php
@@ -3,25 +3,31 @@
    <head>
       <title>ulTIM8</title>
       <link rel="stylesheet" type="text/css" href="/style/home.css">
+      <link rel="stylesheet" type="text/css" href="/style/device.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-      <script src="/jslib/parallax.min.js"></script>
+      <script src="/jslib/parallax.min.js"></script
    </head>
    <body>
       <div id="supercontainer">
          <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/topbar.html"; ?>
          <div id="contentwrapper" class="parallax-window" data-parallax="scroll" data-image-src="/pictures/bg-scroll-sea-2.jpg">
 			   <div id="side">
-				   <a href="#presentazione"> Presentazione</a><br>
-					<a href="#caratteristiche"> Caratteristiche tecniche</a>
+				   <a href="#"> Presentazione</a><br>
+					<a href="#"> Caratteristiche tecniche</a>
 				</div>
             <div id="maincontent">
-				<div id="presentazione" class="item">
+				<div id="presentazione">
+				   <img src="/pictures/products/devices/1.jpg" class= "device-img"></img>
 				   <span class="name"> Modem adsl wi-fi </span><br>
 					<span class="price"> 69 € </span>
+					<p class="description"> Il nuovo modem  </p>  
 				</div>
-				<div id="caratteristiche" class="item">
-				   <span class="descrizione"> Tutto quello che vuoi </span>
-				
+				<div id="caratteristiche">
+				   <span class="specifiche"> Tutto quello che vuoi </span>
+				</div>
+				<div id="link">
+				   <a href="#"> Servizi Smart Life</a><br>
+					<a href="#"> Servizio di assistenza dedicato</a>
 				</div>
             </div>
          </div>
