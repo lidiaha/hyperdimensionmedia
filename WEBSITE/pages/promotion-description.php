2b0a7157176708f716bed44c80ba56d66b22b2d2
michele-mada <michele.mm1234@gmail.com>
2016-06-13 22:10:50 +0200

    changed some filenames because for some reason my browser hates the filename 'promotion'

--- WEBSITE/pages/promotion-description.php
+++ WEBSITE/pages/promotion-description.php
@@ -3,7 +3,7 @@
    <head>
       <title>ulTIM8</title>
       <link rel="stylesheet" type="text/css" href="/style/home.css">
-		<link rel="stylesheet" type="text/css" href="/style/promotion.css">
+      <link rel="stylesheet" type="text/css" href="/style/promotionpage.css">
       <script src="/jslib/jquery-1.11.0.min.js"></script>
       <script src="/jslib/parallax.min.js"></script>
       <script src="/js/home.js"></script>
@@ -14,19 +14,19 @@
          <div id="contentwrapper" class="parallax-window" data-parallax="scroll" data-image-src="/pictures/bg-scroll-sea-2.jpg">
             <div id="maincontent">
                <div class="header">
-					   <div class="name">PROMO TIM VISION con decoder</div>
-						<div class="rules"></div>
-						<div class="mini-description"> Cartoni, cinema e serie TV, dove e quando vuoi</div>
-						<div class="price"> 5€/mese</div>
-						<div class="attiva"> ATTIVA </div>
-					</div>
-					<div class="description"><p>Con TIMvision puoi scegliere tra cinema, serie tv, cartoni e documentari, gli ultimi 7 giorni dei principali canali Rai, La7 e l’archivio La7, dove e quando vuoi.
+                  <div class="name">PROMO TIM VISION con decoder</div>
+                  <div class="rules"></div>
+                  <div class="mini-description"> Cartoni, cinema e serie TV, dove e quando vuoi</div>
+                  <div class="price"> 5€/mese</div>
+                  <div class="attiva"> ATTIVA </div>
+               </div>
+               <div class="description"><p>Con TIMvision puoi scegliere tra cinema, serie tv, cartoni e documentari, gli ultimi 7 giorni dei principali canali Rai, La7 e l’archivio La7, dove e quando vuoi.
                   Più di 8.000 titoli disponibili, sempre on demand, in un solo abbonamento mensile, per creare il tuo palinsesto senza interruzioni pubblicitarie.
                   In più, nella sezione Videostore, trovi gli ultimi successi cinematografici con un catalogo sempre aggiornato, da noleggiare o acquistare in qualsiasi momento anche senza abbonamento.</p>
                </div>
-					<div class="SL"><a href="#"> Scopri di più sul servizio</a></div>
-					<div class="table"></div>
-					<div class="doorstopper"></div>
+               <div class="SL"><a href="#"> Scopri di più sul servizio</a></div>
+               <div class="table"></div>
+               <div class="doorstopper"></div>
             </div>
          </div>
       <!-- <div id="footer"> -->
