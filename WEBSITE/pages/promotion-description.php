95cb9f799cc36ae317395e3ae23814685160c4b3
lidiaha <lidia_93@alice.it>
2016-06-13 21:15:37 +0200

    promotion presentation static

---
+++ WEBSITE/pages/promotion-description.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>ulTIM8</title>
+      <link rel="stylesheet" type="text/css" href="/style/home.css">
+		<link rel="stylesheet" type="text/css" href="/style/promotion.css">
+      <script src="/jslib/jquery-1.11.0.min.js"></script>
+      <script src="/jslib/parallax.min.js"></script>
+      <script src="/js/home.js"></script>
+   </head>
+   <body>
+      <div id="supercontainer">
+         <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/topbar.html"; ?>
+         <div id="contentwrapper" class="parallax-window" data-parallax="scroll" data-image-src="/pictures/bg-scroll-sea-2.jpg">
+            <div id="maincontent">
+               <div class="header">
+					   <div class="name">PROMO TIM VISION con decoder</div>
+						<div class="rules"></div>
+						<div class="mini-description"> Cartoni, cinema e serie TV, dove e quando vuoi</div>
+						<div class="price"> 5€/mese</div>
+						<div class="attiva"> ATTIVA </div>
+					</div>
+					<div class="description"><p>Con TIMvision puoi scegliere tra cinema, serie tv, cartoni e documentari, gli ultimi 7 giorni dei principali canali Rai, La7 e l’archivio La7, dove e quando vuoi.
+                  Più di 8.000 titoli disponibili, sempre on demand, in un solo abbonamento mensile, per creare il tuo palinsesto senza interruzioni pubblicitarie.
+                  In più, nella sezione Videostore, trovi gli ultimi successi cinematografici con un catalogo sempre aggiornato, da noleggiare o acquistare in qualsiasi momento anche senza abbonamento.</p>
+               </div>
+					<div class="SL"><a href="#"> Scopri di più sul servizio</a></div>
+					<div class="table"></div>
+					<div class="doorstopper"></div>
+            </div>
+         </div>
+      <!-- <div id="footer"> -->
+      </div>
+     <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/social-icons.html"; ?>
+   </body>
+</html>
