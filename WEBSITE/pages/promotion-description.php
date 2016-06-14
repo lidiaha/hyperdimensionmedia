70b295bcecfcdd84ba41b45f471f61b6cd205142
lidiaha <lidia_93@alice.it>
2016-06-14 15:14:22 +0200

    table added to promotion page

--- WEBSITE/pages/promotion-description.php
+++ WEBSITE/pages/promotion-description.php
@@ -26,7 +26,38 @@
                   In più, nella sezione Videostore, trovi gli ultimi successi cinematografici con un catalogo sempre aggiornato, da noleggiare o acquistare in qualsiasi momento anche senza abbonamento.</p>
                </div>
                <div class="SL"><a href="#"> Scopri di più sul servizio</a></div>
-               <div class="table"></div>
+               <div class="table">
+					   <table class="table table-striped table-hover ">
+	                  <tbody>
+	    	               <tr>
+			     	            <td>Contributo di attivazione</td>
+			                  <td>nessuno</td>
+                           <td></td>
+			               <thead>
+	                        <tr>
+			                  <th class="col-sm-3">Voci di Costo
+							      </th>
+			                  <th class="col-sm-3">Costi di Listino
+						         </th>
+								   <th class="col-sm-3">Promozioni
+								   </th>
+			                  <th class="col-sm-3">Note
+						    	   </th>
+			                  </tr>
+	                     </thead>
+								<td></td>
+			               </tr>
+		                  <tr>
+			     	            <td>Offerta TIMvision</td>
+			                  <td>10 €/mese</td>
+			                  <td>5 €/mese con decoder incluso</td>
+			                  <td>
+				         		   <p class="note" style="margin:0;">Per tutti i Clienti con linea fissa TIM sia nuovi che già clienti che attivano TIMvision entro il 30/06/2016</p>
+							      </td>
+			               </tr>
+		               </tbody>
+	               </table>
+					</div>
                <div class="doorstopper"></div>
             </div>
          </div>
