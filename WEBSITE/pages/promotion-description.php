63a056bb285a8a82ba7d6ffa98fcbefa92a33848
lidiaha <lidia_93@alice.it>
2016-06-15 09:37:51 +0200

    promotion improved

--- WEBSITE/pages/promotion-description.php
+++ WEBSITE/pages/promotion-description.php
@@ -13,52 +13,47 @@
          <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/topbar.html"; ?>
          <div id="contentwrapper" class="parallax-window" data-parallax="scroll" data-image-src="/pictures/bg-scroll-sea-2.jpg">
             <div id="maincontent">
-               <div class="dummyheader"></div>
-               <div class="header">
-                  <div class="name">PROMO TIM VISION con decoder</div>
-                  <div class="rules"></div>
-                  <div class="mini-description"> Cartoni, cinema e serie TV, dove e quando vuoi</div>
-                  <div class="price"> 5€/mese</div>
-                  <div class="attiva"> ATTIVA </div>
-               </div>
-               <div class="description"><p>Con TIMvision puoi scegliere tra cinema, serie tv, cartoni e documentari, gli ultimi 7 giorni dei principali canali Rai, La7 e l’archivio La7, dove e quando vuoi.
-                  Più di 8.000 titoli disponibili, sempre on demand, in un solo abbonamento mensile, per creare il tuo palinsesto senza interruzioni pubblicitarie.
-                  In più, nella sezione Videostore, trovi gli ultimi successi cinematografici con un catalogo sempre aggiornato, da noleggiare o acquistare in qualsiasi momento anche senza abbonamento.</p>
-               </div>
-               <div class="SL"><a href="#"> Scopri di più sul servizio</a></div>
-               <div class="table">
-					   <table class="table table-striped table-hover ">
-	                  <tbody>
-	    	               <tr>
-			     	            <td>Contributo di attivazione</td>
-			                  <td>nessuno</td>
-                           <td></td>
-			               <thead>
-	                        <tr>
-			                  <th class="col-sm-3">Voci di Costo
-							      </th>
-			                  <th class="col-sm-3">Costi di Listino
-						         </th>
-								   <th class="col-sm-3">Promozioni
-								   </th>
-			                  <th class="col-sm-3">Note
-						    	   </th>
-			                  </tr>
-	                     </thead>
-								<td></td>
-			               </tr>
-		                  <tr>
-			     	            <td>Offerta TIMvision</td>
-			                  <td>10 €/mese</td>
-			                  <td>5 €/mese con decoder incluso</td>
-			                  <td>
-				         		   <p class="note" style="margin:0;">Per tutti i Clienti con linea fissa TIM sia nuovi che già clienti che attivano TIMvision entro il 30/06/2016</p>
-							      </td>
-			               </tr>
-		               </tbody>
-	               </table>
-					</div>
-               <div class="doorstopper"></div>
+				   <?php
+                  include $_SERVER['DOCUMENT_ROOT'] . "/phplib/database.php";
+                  $conn = dbconn();
+
+                  $promo_id = mysqli_real_escape_string($conn, $_GET["promo_id"]);
+						$sql = "SELECT * FROM promotions WHERE id='$promo_id' ";
+                  $result = $conn->query($sql);
+                  if (!$result) {
+                     echo "query error";
+                  }
+                  else {
+                     while($row = $result->fetch_assoc()) {
+                        $name = $row["name"];
+							   $price = $row["price"];
+								$duration = $row["duration"];
+								$description = $row["description"];
+								$subtitle = $row["subtitle"];
+								$table_code = $row["table_code"]; 
+                        echo "<div class='dummyheader'></div>\n";
+                        echo "<div class='header'>\n";
+                        echo "<div class='name'>$name</div>\n";
+                        echo "<div class='rules'></div>\n";
+                        echo "<div class='mini-description'>$subtitle</div>\n";
+                        echo "<div class='price'> $price €/mese";
+							   if ($duration!=0){
+								   echo "per $duration mesi";
+								}
+								echo "</div>\n<div class='attiva'> ATTIVA </div>\n";
+                        echo "</div>\n";
+                        echo "<div class='description'><p>$description</p>\n";
+                        echo "</div>";
+                        echo "<div class='SL'><a href='#'> Scopri di più sul servizio</a></div>\n";
+                        echo "<div class='table'>\n";
+							   echo "$table_code";
+					         echo "</div>";
+			               echo "<div class='doorstopper'></div>\n";
+							}
+                     echo "\n";
+                  }
+                  $conn->close();
+               ?>
             </div>
          </div>
       <!-- <div id="footer"> -->
