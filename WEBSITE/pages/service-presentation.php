86735014b047d15f8fd365dca448903cb3f62da2
lidiaha <lidia_93@alice.it>
2016-06-17 17:15:47 +0200

    added sl service presentation

--- WEBSITE/pages/service-presentation.php
+++ WEBSITE/pages/service-presentation.php
@@ -3,7 +3,7 @@
    <head>
       <title>ulTIM8</title>
       <link rel="stylesheet" type="text/css" href="/style/home.css">
-      <link rel="stylesheet" type="text/css" href="/style/promotionpage.css">
+      <link rel="stylesheet" type="text/css" href="/style/SLpage.css">
       <script src="/jslib/jquery-1.11.0.min.js"></script>
       <script src="/jslib/parallax.min.js"></script>
    </head>
@@ -16,8 +16,15 @@
                   include $_SERVER['DOCUMENT_ROOT'] . "/phplib/database.php";
                   $conn = dbconn();
 
-                  $promo_id = mysqli_real_escape_string($conn, $_GET["promo_id"]);
-                  $sql = "SELECT * FROM promotions WHERE id='$promo_id' ";
+                  $service_id = mysqli_real_escape_string($conn, $_GET["service_id"]);
+						
+						function printUrls($url) {
+                     $links = explode(";",$url);
+                     foreach($links as $link){
+                        echo "<a href='$link'><div class='link'></div></a>";
+                     }
+                  }
+                  $sql = "SELECT * FROM sl_services WHERE id='$service_id' ";
                   $result = $conn->query($sql);
                   if (!$result) {
                      echo "query error";
@@ -25,28 +32,16 @@
                   else {
                      while($row = $result->fetch_assoc()) {
                         $name = $row["name"];
-                        $price = $row["price"];
-                        $duration = $row["duration"];
                         $description = $row["description"];
-                        $subtitle = $row["subtitle"];
-                        $table_code = $row["table_code"];
+								$urls = $row["url"];
                         echo "<div class='dummyheader'></div>\n";
-                        echo "<div class='header' style='background-image: url(\"/pictures/promotionbanners/$promo_id.jpg\")'>\n";
+                        echo "<div class='header' style='background-image: url(\"/pictures/products/servicesbanners/$service_id.jpg\")'>\n";
                         echo "<div class='name'>$name</div>\n";
-                        echo "<div class='rules'></div>\n";
-                        echo "<div class='mini-description'>$subtitle</div>\n";
-                        echo "<div class='price'> $price €/mese";
-                        if ($duration!=0){
-                           echo "per $duration mesi";
-                        }
-                        echo "</div>\n<div class='attiva'> ATTIVA </div>\n";
-                        echo "</div>\n";
-                        echo "<div class='description'><p>$description</p>\n";
+                        echo "<div class='description'><p>$description</p></div>\n";
                         echo "</div>";
-                        echo "<div class='SL'><a href='#'> Scopri di più sul servizio</a></div>\n";
-                        echo "<div class='table'>\n";
-                        echo "$table_code";
-                        echo "</div>";
+								printUrls($urls);
+                        echo "<div class='products scopri'><a href='#'> Scopri i prodotti</a></div>\n";
+								echo "<div class='offers scopri'><a href='#'> Scopri le offerte</a></div>\n";
                         echo "<div class='doorstopper'></div>\n";
                      }
                      echo "\n";
