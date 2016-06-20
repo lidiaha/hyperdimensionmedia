2147926827c6f571ab73dca8c354d01c274e0b5c
michele-mada <michele.mm1234@gmail.com>
2016-06-20 12:28:29 +0200

    linked promotion to respective sl service

--- WEBSITE/pages/promotion-description.php
+++ WEBSITE/pages/promotion-description.php
@@ -15,6 +15,22 @@
                <?php
                   include $_SERVER['DOCUMENT_ROOT'] . "/phplib/database.php";
                   include $_SERVER['DOCUMENT_ROOT'] . "/phplib/image-auto-extension.php";
+
+                  function getSLServiceID($conn, $promo_id) {
+                     $sql = "SELECT * FROM service_promo WHERE promo_id='$promo_id' ";
+                     $result = $conn->query($sql);
+                     if (!$result) {
+                        echo "query error";
+                     }
+                     else {
+                        while($row = $result->fetch_assoc()) {
+                           $service_id = $row["service_id"];
+                           return $service_id;
+                        }
+                     }
+                     return -1;
+                  }
+
                   $conn = dbconn();
 
                   $promo_id = mysqli_real_escape_string($conn, $_GET["promo_id"]);
@@ -45,7 +61,12 @@
                         echo "</div>\n";
                         echo "<div class='description'><p>$description</p>\n";
                         echo "</div>";
-                        echo "<div class='SL'><a href='#'> Scopri di più sul servizio</a></div>\n";
+                        $linked_sl = getSLServiceID($conn, $promo_id);
+                        if ($linked_sl < 0) {
+                           echo "<div class='SL'>Nessun servizio Smart Life associato</div>\n";
+                        } else {
+                           echo "<div class='SL'><a href='/pages/service-presentation.php?service_id=$linked_sl'> Scopri di più sul servizio</a></div>\n";
+                        }
                         echo "<div class='table'>\n";
                         echo "$table_code";
                         echo "</div>";
