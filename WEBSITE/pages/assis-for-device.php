7c86b0e7aacef6d7cf3b4b1b8e95638c6bd3df62
lidiaha <lidia_93@alice.it>
2016-06-22 19:15:51 +0200

    tag system 1.0

---
+++ WEBSITE/pages/assis-for-device.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <link rel="stylesheet" type="text/css" href="/style/home.css" media="screen and (min-width: 480px)">
+      <link rel="stylesheet" type="text/css" href="/style/mobile/home.css" media="screen and (max-width: 480px)">
+      <script src="/jslib/jquery-1.11.0.min.js"></script>
+      <script src="/jslib/parallax.min.js"></script>
+      <?php
+         include_once $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/page-identify.php";
+         pageIdentify("related assistance");
+       ?>
+   </head>
+   <body>
+      <div id="supercontainer">
+         <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/topbar.html"; ?>
+         <div id="contentwrapper" class="parallax-window" data-parallax="scroll" data-image-src="/pictures/bg-scroll-sea-2.jpg">
+            <div id="maincontent">
+				   <?php
+                  include_once $_SERVER['DOCUMENT_ROOT'] . "/phplib/database.php";
+                  $conn = dbconn();
+                  $conn2 = dbconn();
+						$array = array();
+                  $device_id = mysqli_real_escape_string($conn, $_GET["device_id"]);
+						
+						function findMatch($tags, $tags2 , $id) {
+                     if($tags!=null){
+                        foreach($tags as $tag){
+                           foreach($tags2 as $tag){
+									   if($tag==$tag2){
+											array_push($array, $id);
+											break;
+										}
+									}
+                        }
+                     }
+                  }
+               ?>
+               <div class="gobackbar" onclick="location.href='/pages/device-presentation.php?device_id=<?php echo $device_id; ?>'">
+                  <div class="arrowback"></div>
+                  <div class="labelback">Torna al prodotto</div>
+               </div>
+               <?php
+                  $sql = "SELECT typetags FROM devices WHERE device_id='$device_id'";
+                  $result = $conn->query($sql);
+                  if (!$result) {
+                     echo "query error";
+                  }
+                  else {
+                     while($row = $result->fetch_assoc()) {
+                        $typetags= $row["typetags"];
+                        $tags = explode(";",$typetags);
+								$sql2 = "SELECT * FROM assistance";
+                        $result2 = $conn2->query($sql2);
+                        if (!$result2) {
+                           echo "query error";
+                        }
+                        else {
+									while($row2 = $result2->fetch_assoc()) {
+										$typetags2= $row2["typetags"];
+										$id_as= $row2["id"];
+                              $tags2 = explode(";",$typetags2);
+								      findMatch($tags, $tags2, $id_as);
+										
+								}
+                        echo "<div class='doorstopper'></div>\n";
+                     }
+                     echo "\n";
+                     if (mysqli_num_rows($result) == 0) {
+                        include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/no-results.html";
+                     }
+                  }
+                  $conn->close();
+               ?>
+            </div>
+         </div>
+      <!-- <div id="footer"> -->
+      </div>
+     <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/social-icons.html"; ?>
+   </body>
+</html>
