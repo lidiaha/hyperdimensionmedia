2766e2ced0295be410d9909a7ad81d707bd7760b
lidiaha <lidia_93@alice.it>
2016-06-22 18:08:38 +0200

    added assistance toggle

--- WEBSITE/js/toggle.js
+++ WEBSITE/js/toggle.js
@@ -1,6 +1,13 @@
-
-         $(document).ready(function() {
-            $(".assis_category").click(function() {
-               $(this).parent().find(".assis_subcategory").toggle();
-            });
-         });
+if (parseInt($(window).width()) <= 480) {
+   $(document).ready(function() {
+	   $(document).on("click", ".assis_category_label", function() {
+      $(this).parent().find(".assis_subcategory").toggle();
+	   });
+	   $(document).on("click", ".assis_subcategory_label", function() {
+      $(this).parent().find(".assis_subtopic").toggle();
+	   });
+	   $(document).on("click", ".assis_subtopic_label", function() {
+      $(this).parent().find(".assis_item").toggle();
+	   });
+   });
+}   
