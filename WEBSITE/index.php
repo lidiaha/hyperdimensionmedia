81900fbe9a04c2457353762846691287829fde0f
lidiaha <lidia_93@alice.it>
2016-06-23 11:01:09 +0200

    home progress

--- WEBSITE/index.php
+++ WEBSITE/index.php
@@ -4,6 +4,7 @@
       <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/viewport.html"; ?>
       <link rel="stylesheet" type="text/css" href="/style/home.css" media="screen and (min-width: 480px)">
       <link rel="stylesheet" type="text/css" href="/style/mobile/home.css" media="screen and (max-width: 480px)">
+		<link rel="stylesheet" type="text/css" href="/style/homepage.css" media="screen and (min-width: 480px)">
       <script src="/jslib/jquery-1.11.0.min.js"></script>
       <script src="/jslib/parallax.min.js"></script>
       <script src="/js/home.js"></script>
@@ -20,24 +21,29 @@
                <?php include $_SERVER['DOCUMENT_ROOT'] . "/ui-elements/ElasticSlideshow/index2.html"; ?>
 					<div class="trending">
 					   <div class="top outlet">
-						   <div class="pic"></div>
-							<div class="name">Prodotti scontati</div>
+						   <div class="pic" style="background-image: url(/pictures/sconti.jpg)">
+							   <div class="name">Prodotti scontati</div>
+							</div>
 						</div>
 						<div class="top">
-						   <div class="pic"></div>
-							<div class="name">Internet senza limiti</div>
+						   <div class="pic" style="background-image: url(/pictures/top2.jpg)">
+							   <div class="name">Internet senza limiti</div>
+							</div>
 						</div>
 						<div class="top">
-						   <div class="pic"></div>
-							<div class="name">Netflix</div>
+						   <div class="pic" style="background-image: url(/pictures/top3.jpg)">
+							   <div class="name">Netflix</div>
+							</div>
 						</div>
 						<div class="top">
-						   <div class="pic"></div>
-							<div class="name">Tim Smart Fibra</div>
+						   <div class="pic" style="background-image: url(/pictures/top4.jpg)">
+							   <div class="name">Tim Smart Fibra</div>
+							</div>
 						</div>
 				   </div> 
 					<div class="util">
 					</div>
+					<div class='doorstopper'></div>
             </div>
          </div>
       <!-- <div id="footer"> -->
