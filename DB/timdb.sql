ce9bb0a58fbacebbc9b62814df1f5c3a75d2975d
michele-mada <michele.mm1234@gmail.com>
2016-06-24 20:31:43 +0200

    fixed wrong filenames

--- DB/timdb.sql
+++ DB/timdb.sql
@@ -564,9 +564,10 @@ CREATE TABLE `page_hits` (
 
 LOCK TABLES `page_hits` WRITE;
 /*!40000 ALTER TABLE `page_hits` DISABLE KEYS */;
-INSERT INTO `page_hits` VALUES (7,'devices',5);
-INSERT INTO `page_hits` VALUES (8,'devices',2);
-INSERT INTO `page_hits` VALUES (14,'devices',1);
+INSERT INTO `page_hits` VALUES (7,'devices',12);
+INSERT INTO `page_hits` VALUES (8,'devices',3);
+INSERT INTO `page_hits` VALUES (11,'devices',1);
+INSERT INTO `page_hits` VALUES (14,'devices',2);
 INSERT INTO `page_hits` VALUES (16,'devices',1);
 /*!40000 ALTER TABLE `page_hits` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -793,4 +794,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-24 20:24:21
+-- Dump completed on 2016-06-24 20:31:43
