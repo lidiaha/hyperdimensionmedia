e90b6384dd6bbbf6c7c260ccb92389ba15721c8d
lidiaha <lidia_93@alice.it>
2016-06-25 14:41:20 +0200

    change color

--- WEBSITE/js/SLpage.js
+++ WEBSITE/js/SLpage.js
@@ -1,12 +1,14 @@
 
-var dark_cutoff = 120.0;
-var lightcolor = "#7cff70";
+var dark_cutoff = 170.0;
+var lightcolor = "#FFDEAD";
 
 
 function afterDataLoaded() {
    var brightness = $(".header").data("brightness");
    if (brightness < dark_cutoff) {
       $(".name").css("color", lightcolor);
-      $(".description").css("color", lightcolor);
+		$(".name").css("text-shadow", "2px 1px #191970");
+		$(".description").css("color", lightcolor);
+		$(".description").css("text-shadow", "-1px -1px 0 #191970,1px -1px 0 #191970,-1px 1px 0 #191970,1px 1px 0 #191970");
    }
 }
