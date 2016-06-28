4c11ca5e5220a101d3f7bb739abef10d10c53591
michele-mada <michele.mm1234@gmail.com>
2016-06-28 21:47:37 +0200

    configured database.php for production/developement modes

--- WEBSITE/phplib/database.php
+++ WEBSITE/phplib/database.php
@@ -1,16 +1,28 @@
 <?php
+
+$dbconfigmode = "developement";
+
 function dbconn() {
-    $servername = "localhost";
-    $username = "timuser";
-    $password = "timuser";
-    $dbname = "timdb";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    mysqli_set_charset($conn, 'utf8');
-    return $conn;
+   global $dbconfigmode;
+   if ($dbconfigmode == "developement"){
+      $servername = "localhost";
+      $username = "timuser";
+      $password = "timuser";
+      $dbname = "timdb";
+   } else { // production
+      $servername = "localhost";
+      $username = "<username here>";
+      $password = "<password here>";
+      $dbname = "my_ultim8";
+   }
+
+   // Create connection
+   $conn = new mysqli($servername, $username, $password, $dbname);
+   // Check connection
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+   }
+   mysqli_set_charset($conn, 'utf8');
+   return $conn;
 }
  ?>
