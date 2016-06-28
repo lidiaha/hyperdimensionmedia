9f33a7aea2795f62b41bd841eb4ff23acbb22b9c
michele-mada <michele.mm1234@gmail.com>
2016-06-28 22:54:18 +0200

    mega-oneliner

--- DB/timdb.sql
+++ DB/timdb.sql
@@ -566,7 +566,7 @@ CREATE TABLE `page_hits` (
 LOCK TABLES `page_hits` WRITE;
 /*!40000 ALTER TABLE `page_hits` DISABLE KEYS */;
 INSERT INTO `page_hits` VALUES (2,'devices',6);
-INSERT INTO `page_hits` VALUES (7,'devices',28);
+INSERT INTO `page_hits` VALUES (7,'devices',29);
 INSERT INTO `page_hits` VALUES (8,'devices',4);
 INSERT INTO `page_hits` VALUES (9,'devices',1);
 INSERT INTO `page_hits` VALUES (10,'devices',1);
@@ -825,4 +825,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-28 22:36:04
+-- Dump completed on 2016-06-28 22:54:18
