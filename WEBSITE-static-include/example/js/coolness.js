0cc041d011a010986c5d121be607146bd8c833d6
michele-mada <michele.mm1234@gmail.com>
2016-06-29 18:11:33 +0200

    structure fix

--- WEBSITE-static-include/example/js/coolness.js
+++
@@ -1,6 +0,0 @@
-function showCoolness(val) {
-  if (val >= 8) document.write("Sub-zero");
-  else if (val > 5) document.write("Cool");
-  else if (val >= 3) document.write("Un-cool");
-  else document.write("Seriously Un-cool");
-}
